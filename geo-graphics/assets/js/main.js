e9ea2f26f380376bb9f07032736aea1f37285799
James <james.t.highfield@gmail.com>
2019-09-12 09:40:06 +1000

    GeoGraphics Update

---
+++ geo-graphics/assets/js/main.js
@@ -0,0 +1,15 @@
+setTimeout(function() {
+  $(".face-text").addClass("show");
+}, 3000);
+
+setTimeout(function() {
+  $(".face-text").addClass("hide");
+}, 6000);
+
+setTimeout(function() {
+  $(".face-text").addClass("show");
+}, 6000);
+
+setTimeout(function() {
+  $(".face-text").addClass("hide");
+}, 12000);
