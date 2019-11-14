2bbe827d9fb1b97329f709a9811952756e0ab267
James <james.t.highfield@gmail.com>
2019-11-14 16:36:47 +1100

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+$('.public-domain').click(function() {
+	$('.left-content').removeClass('popup-show');
+  	$('.left-content-pd').addClass('popup-show');
+});
+
+$('.pd-return').click(function() {
+	$('.left-content').removeClass('popup-show');
+  	$('.left-content-primary').addClass('popup-show');
+});
+
 $('.index-killer').click(function() {
 	$('.right-content').removeClass('popup-show');
   	$('.right-killer').addClass('popup-show');
@@ -9,6 +19,11 @@ $('.index-killer').click(function() {
 $('.index-monster').click(function() {
  	 $('.right-content').removeClass('popup-show');
   	$('.right-monster').addClass('popup-show');
+});
+
+$('.index-destroy').click(function() {
+ 	 $('.right-content').removeClass('popup-show');
+  	$('.right-destroy').addClass('popup-show');
 });
 
 $('.index-ufo').click(function() {
@@ -24,6 +39,16 @@ $('.index-doomsday').click(function() {
 $('.index-beyond').click(function() {
  	 $('.right-content').removeClass('popup-show');
   	$('.right-beyond').addClass('popup-show');
+});
+
+$('.index-plan').click(function() {
+ 	 $('.right-content').removeClass('popup-show');
+  	$('.right-plan').addClass('popup-show');
+});
+
+$('.index-phantom').click(function() {
+ 	 $('.right-content').removeClass('popup-show');
+  	$('.right-phantom').addClass('popup-show');
 });
 
 
